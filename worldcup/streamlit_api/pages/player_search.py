e84fda5b7dd6879a114088deb892a94d92b3017a
drs650 <huborobot@gmail.com>
2026-08-28 12:21:00 +0900

    3차 수정

--- worldcup/streamlit_api/pages/player_search.py
+++ worldcup/streamlit_api/pages/player_search.py
@@ -48,11 +48,9 @@ with st.form("player_search_form"):
         sort_by = st.selectbox(
             "정렬 기준",
             [
-                "id",
                 "player",
-                "team",
+                "games",
                 "age",
-                "appearances",
                 "minutes",
                 "goals",
                 "assists",
@@ -124,12 +122,10 @@ if data:
     display_columns = [
         column
         for column in [
-            "id",
+
             "player",
-            "team",
-            "position",
+            "games",
             "age",
-            "appearances",
             "minutes",
             "goals",
             "assists",
